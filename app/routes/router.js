b93b6406def2e8e5b454fcdb2a8d2915085dbd8c
Sanea+ <carol2711campos@gmail.com>
2024-04-02 11:07:14 -0300

    01

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,37 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const moment = require("moment");
-const tarefasController = require("../controllers/tarefasController");
 
-router.get("/",  function (req, res) {
-   tarefasController.listarTarefasPaginadas(req, res);
+const pool = require("../../config/pool_conexoes")
+
+router.get("/", function (req, res) {
+  res.render("pages/index", {pagina:"home", logado:null});
 });
 
-router.get("/editar", function (req, res) {
-  tarefasController.exibirTarefaId(req, res);
-});
-
-router.get("/excluir", function (req, res) {
-  tarefasController.excluirTarefa(req, res);
-});
-
-router.get("/finalizar", function (req, res) {
-  tarefasController.finalizarTarefa(req, res);
-});
-
-router.get("/iniciar", function (req, res) {
-  tarefasController.iniciarTarefa(req, res);
-});
-
-router.get("/adicionar", function (req, res) {
-  res.locals.moment = moment;
-  res.render("pages/adicionar", { dados: null, listaErros: null });
-});
-
-router.post("/adicionar", tarefasController.regrasValidacao, function (req, res) {
-    tarefasController.adicionarTarefa(req, res);
-  }
-);
 
 
 module.exports = router;
