11348e6e12289d2f8504ee0233caf343f783addf
Sanea+ <carol2711campos@gmail.com>
2024-04-03 11:01:39 -0300

    rotas meia boca

--- app/routes/router.js
+++ app/routes/router.js
@@ -7,6 +7,37 @@ router.get("/", function (req, res) {
   res.render("pages/index", {pagina:"home", logado:null});
 });
 
+router.get("/cadastro", function (req, res) {
+  res.render("pages/cadastro/index");
+});
+
+router.get("/login", function (req, res) {
+  res.render("pages/login/index");
+});
+
+router.get("/FaleConoco", function (req, res) {
+  res.render("pages/FaleConoco/index");
+});
+
+router.get("/Serviço", function (req, res) {
+  res.render("pages/Serviço/servicos");
+}); //.esse ta dando erro//
+
+router.get("/Sobre", function (req, res) {
+  res.render("pages/Sobre/sobre");
+});
+
+router.get("/login/index", function (req, res) {
+  res.render("pages/Publicacao/publicacao/index");
+});
+
+router.get("/Publicacao", function (req, res) {
+  res.render("pages/Publicacao/Config/index");
+});
+
+router.get("/Publicacao", function (req, res) {
+  res.render("pages/Publicacao/Perfil/index");
+});
 
 
 module.exports = router;
