f90f4e970f89a1a9ddc1b68cb6bfb2c332e184a1
Sanea+ <carol2711campos@gmail.com>
2024-09-18 11:34:55 -0300

    script bd atualizado

--- config/script_bd.sql
+++ config/script_bd.sql
@@ -1,5 +1,3 @@
--- MySQL Workbench Forward Engineering
- 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -15,155 +13,140 @@ CREATE SCHEMA IF NOT EXISTS `bzudsbddxmqodnzmzk08` DEFAULT CHARACTER SET utf8 ;
 USE `bzudsbddxmqodnzmzk08` ;
  
 -- -----------------------------------------------------
+-- Table `bzudsbddxmqodnzmzk08`.`tipo_usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`tipo_usuario` (
+  `id_usuario` INT NOT NULL AUTO_INCREMENT,
+  `tipo_usuario` VARCHAR(45) NOT NULL,
+  `descricao_usuario` VARCHAR(255) NOT NULL,
+  `status_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_usuario`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8;
+ 
+ 
+-- -----------------------------------------------------
 -- Table `bzudsbddxmqodnzmzk08`.`USUARIOS`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`USUARIOS` ;
- 
 CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`USUARIOS` (
   `id_usuario` INT NOT NULL AUTO_INCREMENT,
   `nome_usuario` VARCHAR(45) NOT NULL,
   `data_nasc_usuario` DATE NOT NULL,
-  `foto_usuario` VARCHAR(45) BINARY NULL,
+  `foto_usuario` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL,
   `cidade_usuario` VARCHAR(45) NOT NULL,
-  `logradouro_usuario` VARCHAR(45) NULL,
-  `bairro_usuario` VARCHAR(45) NULL,
-  `uf_usuario` CHAR(30) NULL,
+  `uf_usuario` CHAR(30) NULL DEFAULT NULL,
   `email_usuario` VARCHAR(150) NOT NULL,
   `senha_usuario` VARCHAR(512) NOT NULL,
   `user_usuario` VARCHAR(45) NOT NULL,
-  `telefone_usuario` CHAR(11) NULL,
-  
+  `telefone_usuario` CHAR(11) NULL DEFAULT NULL,
+  `tipo_usuario_id` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id_usuario`),
   UNIQUE INDEX `idUSUARIOS_UNIQUE` (`id_usuario` ASC) VISIBLE,
   UNIQUE INDEX `email_usuario_UNIQUE` (`email_usuario` ASC) VISIBLE,
-  UNIQUE INDEX `senha_usuario_UNIQUE` (`senha_usuario` ASC) VISIBLE,
   UNIQUE INDEX `user_usuario_UNIQUE` (`user_usuario` ASC) VISIBLE,
-  UNIQUE INDEX `telefone_usuario_UNIQUE` (`telefone_usuario` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-INSERT INTO bzudsbddxmqodnzmzk08.USUARIOS(id_usuario, nome_usuario,cidade_usuario, data_nasc_usuario, email_usuario, senha_usuario, tipo_usuario_id, user_usuario) 
-VALUES (1, 'adm',  'Barueri','2006-04-06','admin@adm.com', '$2a$12$J1piFIoOATeGAlIOPW0HSuo.pWb/tBJabaoyNNMkxwgrLAG70MYFK', 3, 'adm');
-
+  UNIQUE INDEX `telefone_usuario_UNIQUE` (`telefone_usuario` ASC) VISIBLE,
+  INDEX `fk_tipo_usuario` (`tipo_usuario_id` ASC) VISIBLE,
+  CONSTRAINT `fk_tipo_usuario`
+    FOREIGN KEY (`tipo_usuario_id`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`tipo_usuario` (`id_usuario`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
  
  
 -- -----------------------------------------------------
 -- Table `bzudsbddxmqodnzmzk08`.`POSTS`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`POSTS` ;
- 
 CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`POSTS` (
   `id_POSTS` INT NOT NULL AUTO_INCREMENT,
-  `comentarios_posts` VARCHAR(200) NULL,
-  `img_posts` BLOB NULL,
-  `USUARIOS_id_usuario` INT NOT NULL,
-  `titulo` VARCHAR(60) NULL,
+  `comentarios_posts` VARCHAR(255) NULL DEFAULT NULL,
+  `img_posts` BLOB NULL DEFAULT NULL,
+  `USUARIOS_id_usuario1` INT NOT NULL,
   PRIMARY KEY (`id_POSTS`),
   UNIQUE INDEX `idPOSTS_UNIQUE` (`id_POSTS` ASC) VISIBLE,
-  INDEX `fk_POSTS_USUARIOS1_idx` (`USUARIOS_id_usuario` ASC) VISIBLE)
-ENGINE = InnoDB;
- 
- 
--- -----------------------------------------------------
--- Table `bzudsbddxmqodnzmzk08`.`INSTITUICOES`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`INSTITUICOES` ;
- 
-CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`INSTITUICOES` (
-  `id_INSTITUICOES` INT UNSIGNED NOT NULL,
-  `celular_instituicao` CHAR(20) NULL,
-  `email_instituicao` VARCHAR(70) NULL,
-  `cnpj_instituicao` VARCHAR(45) NULL,
-  `razao_social_instituicao` INT NULL,
-  `logo_instituicao` BLOB NULL,
-  `cidade_instituicao` VARCHAR(45) NOT NULL,
-  `logradouro_instituicao` VARCHAR(45) NULL,
-  `bairro_instituicao` VARCHAR(45) NULL,
-  `uf_instituicao` CHAR(30) NULL,
-  PRIMARY KEY (`id_INSTITUICOES`))
-ENGINE = InnoDB;
- 
- 
--- -----------------------------------------------------
--- Table `bzudsbddxmqodnzmzk08`.`COMENTARIOS`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`COMENTARIOS` ;
- 
-CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`COMENTARIOS` (
-  `id_COMENTARIOS` INT NOT NULL,
-  `comentarios_posts` VARCHAR(300) NULL,
-  `comentarios_usuarios` VARCHAR(300) NULL,
-  `comentarios_instituicoes` VARCHAR(300) NULL,
-  `POSTS_id_POSTS` INT NOT NULL,
-  `INSTITUICOES_id_INSTITUICOES` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_COMENTARIOS`),
-  UNIQUE INDEX `idCOMENTARIOS_UNIQUE` (`id_COMENTARIOS` ASC) VISIBLE,
-  INDEX `fk_COMENTARIOS_POSTS1_idx` (`POSTS_id_POSTS` ASC) VISIBLE,
-  INDEX `fk_COMENTARIOS_INSTITUICOES1_idx` (`INSTITUICOES_id_INSTITUICOES` ASC) VISIBLE)
-ENGINE = InnoDB;
+  INDEX `fk_POSTS_USUARIOS1_idx1` (`USUARIOS_id_usuario1` ASC) VISIBLE,
+  CONSTRAINT `fk_POSTS_USUARIOS1`
+    FOREIGN KEY (`USUARIOS_id_usuario1`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`USUARIOS` (`id_usuario`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
  
  
 -- -----------------------------------------------------
 -- Table `bzudsbddxmqodnzmzk08`.`DENUNCIA DE POSTAGENS`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`DENUNCIA DE POSTAGENS` ;
- 
 CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`DENUNCIA DE POSTAGENS` (
-  `id_DENUNCIA_DE_POSTAGENS` INT NOT NULL,
-  `data_denun` DATE NULL,
-  `denuncia` VARCHAR(200) NULL,
-  `USUARIOS_id_usuario` INT NOT NULL,
+  `id_DENUNCIA` INT NOT NULL,
+  `data_denun` DATE NULL DEFAULT NULL,
+  `denuncia` VARCHAR(200) NULL DEFAULT NULL,
   `POSTS_id_POSTS` INT NOT NULL,
-  PRIMARY KEY (`id_DENUNCIA_DE_POSTAGENS`, `POSTS_id_POSTS`),
-  UNIQUE INDEX `idDENUNCIA DE POSTAGENS_UNIQUE` (`id_DENUNCIA_DE_POSTAGENS` ASC) VISIBLE,
-  INDEX `fk_DENUNCIA DE POSTAGENS_USUARIOS1_idx` (`USUARIOS_id_usuario` ASC) VISIBLE,
-  INDEX `fk_DENUNCIA DE POSTAGENS_POSTS1_idx` (`POSTS_id_POSTS` ASC) VISIBLE)
-ENGINE = InnoDB;
+  `USUARIOS_id_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_DENUNCIA`),
+  UNIQUE INDEX `idDENUNCIA DE POSTAGENS_UNIQUE` (`id_DENUNCIA` ASC) VISIBLE,
+  INDEX `fk_DENUNCIA DE POSTAGENS_POSTS1_idx1` (`POSTS_id_POSTS` ASC) VISIBLE,
+  INDEX `fk_DENUNCIA DE POSTAGENS_USUARIOS1_idx1` (`USUARIOS_id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_DENUNCIA DE POSTAGENS_POSTS1`
+    FOREIGN KEY (`POSTS_id_POSTS`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`POSTS` (`id_POSTS`),
+  CONSTRAINT `fk_DENUNCIA DE POSTAGENS_USUARIOS1`
+    FOREIGN KEY (`USUARIOS_id_usuario`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`USUARIOS` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
  
  
 -- -----------------------------------------------------
--- Table `bzudsbddxmqodnzmzk08`.`ADM`
+-- Table `bzudsbddxmqodnzmzk08`.`INSTITUICOES`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`ADM` ;
- 
-CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`ADM` (
-  `id_adm` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id_adm`))
-ENGINE = InnoDB;
- 
- 
--- -----------------------------------------------------
--- Table `bzudsbddxmqodnzmzk08`.`respostas_inst`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`respostas_inst` ;
- 
-CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`respostas_inst` (
-  `id_respostas` INT NOT NULL,
-  `COMENTARIOS_id_COMENTARIOS` INT NOT NULL,
-  `data` DATETIME NULL,
-  `respostascol` VARCHAR(45) NULL,
-  `INSTITUICOES_id_INSTITUICOES` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_respostas`),
-  INDEX `fk_respostas_COMENTARIOS1_idx` (`COMENTARIOS_id_COMENTARIOS` ASC) VISIBLE,
-  INDEX `fk_respostas_inst_INSTITUICOES1_idx` (`INSTITUICOES_id_INSTITUICOES` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`INSTITUICOES` (
+  `id_INSTITUICOES` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `cnpj_instituicao` VARCHAR(45) NULL DEFAULT NULL,
+  `razao_social_instituicao` VARCHAR(255) NULL DEFAULT NULL,
+  `logo_instituicao` BLOB NULL DEFAULT NULL,
+  `USUARIOS_id_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_INSTITUICOES`),
+  INDEX `fk_INSTITUICOES_USUARIOS1_idx` (`USUARIOS_id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_INSTITUICOES_USUARIOS1`
+    FOREIGN KEY (`USUARIOS_id_usuario`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`USUARIOS` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
  
  
 -- -----------------------------------------------------
 -- Table `bzudsbddxmqodnzmzk08`.`respostas_usu`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `bzudsbddxmqodnzmzk08`.`respostas_usu` ;
- 
 CREATE TABLE IF NOT EXISTS `bzudsbddxmqodnzmzk08`.`respostas_usu` (
   `id_respostas` INT NOT NULL,
-  `COMENTARIOS_id_COMENTARIOS` INT NOT NULL,
-  `data` DATETIME NULL,
-  `respostascol` VARCHAR(45) NULL,
+  `resposta` VARCHAR(255) NOT NULL,
+  `data` DATETIME NULL DEFAULT NULL,
   `USUARIOS_id_usuario` INT NOT NULL,
+  `POSTS_id_POSTS` INT NOT NULL,
   PRIMARY KEY (`id_respostas`),
-  INDEX `fk_respostas_COMENTARIOS1_idx` (`COMENTARIOS_id_COMENTARIOS` ASC) VISIBLE,
-  INDEX `fk_respostas_usu_USUARIOS1_idx` (`USUARIOS_id_usuario` ASC) VISIBLE)
-ENGINE = InnoDB;
+  INDEX `fk_respostas_COMENTARIOS1_idx` (`resposta` ASC) VISIBLE,
+  INDEX `fk_respostas_usu_USUARIOS1_idx1` (`USUARIOS_id_usuario` ASC) VISIBLE,
+  INDEX `fk_respostas_usu_POSTS1_idx` (`POSTS_id_POSTS` ASC) VISIBLE,
+  CONSTRAINT `fk_respostas_usu_USUARIOS1`
+    FOREIGN KEY (`USUARIOS_id_usuario`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`USUARIOS` (`id_usuario`),
+  CONSTRAINT `fk_respostas_usu_POSTS1`
+    FOREIGN KEY (`POSTS_id_POSTS`)
+    REFERENCES `bzudsbddxmqodnzmzk08`.`POSTS` (`id_POSTS`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
  
+ INSERT INTO bzudsbddxmqodnzmzk08.USUARIOS(id_usuario, nome_usuario,cidade_usuario, data_nasc_usuario, email_usuario, senha_usuario, tipo_usuario_id, user_usuario)
+VALUES (1, 'adm',  'Barueri','2006-04-06','admin@adm.com', '$2a$12$J1piFIoOATeGAlIOPW0HSuo.pWb/tBJabaoyNNMkxwgrLAG70MYFK', 3, 'adm');
+ 
+INSERT INTO bzudsbddxmqodnzmzk08.USUARIOS(id_usuario, nome_usuario,cidade_usuario, data_nasc_usuario, email_usuario, senha_usuario, tipo_usuario_id, user_usuario)
+VALUES (2, 'comum',  'Barueri','2006-04-09','comum@teste.com', '$2a$12$J1piFIoOATeGAlIOPW0HSuo.pWb/tBJabaoyNNMkxwgrLAG70MYFK', 1, 'comum');
  
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
