a1e738dd3c2a830394c56ea3b3130bb45a83a8c9
Sanea+ <carol2711campos@gmail.com>
2024-09-20 21:39:36 -0300

    adm estrutura

---
+++ app/controllers/admCrontroller.js
@@ -0,0 +1,65 @@
+const admModel = require('../models/admModel.js');
+
+const admController = {
+    listAllUsers: async (req, res) => {
+        try {
+            let pagina = req.query.pagina == undefined ? 1 : parseInt(req.query.pagina);
+            let regPagina = 2;
+            let inicio = (pagina - 1) * regPagina;
+
+            let totReg = await admModel.totalReg();
+            let totPaginas = Math.ceil(totReg[0].total / regPagina);
+
+            let users = await admModel.findPage(inicio, regPagina) || [];
+
+
+            let paginador = totReg[0].total <= regPagina ? null : { 
+                "pagina_atual": pagina, 
+                "total_reg": totReg[0].total, 
+                "total_paginas": totPaginas 
+            };
+
+            // Salvando a notificação em uma variável temporária e remover da sessão
+            const notificacao = req.session.dadosNotificacao || null;
+            if (req.session.dadosNotificacao) {
+                delete req.session.dadosNotificacao;
+            }
+
+            // Renderizar a página 
+            res.render("adm", {
+                autenticado: req.session.autenticado,
+                users: users, // Atualizado para usar o resultado paginado
+                dadosNotificacao: notificacao,
+                paginador: paginador
+            });
+
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+    //Excluir usuário da tabela 
+    deleteUse: async (req, res) => {
+        const { id, type } = req.params;
+        try {
+            await admModel.findByTypeDelete(id);
+            
+            // Notificação pode ser salva na sessão, se necessário
+            req.session.dadosNotificacao = {
+                titulo: "Sucesso",
+                mensagem: "Usuário deletado com sucesso",
+                tipo: "success"
+            };
+    
+            if (type == '1') {
+                res.redirect('/adm');
+            }
+        } catch (error) {
+            res.status(500).json({ error: "Erro ao deletar o usuário" });
+        }
+    }
+    
+}
+
+
+
+module.exports = admController;
