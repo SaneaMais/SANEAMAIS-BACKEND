c887b631401dff191e1ff9c1db00a183b03a46c8
Sanea+ <carol2711campos@gmail.com>
2024-10-01 18:22:00 -0300

    .

--- app/controllers/InstituController.js
+++ app/controllers/InstituController.js
@@ -2,14 +2,16 @@ const InstituicaoModel = require('../models/InstituModel');
 
 class InstituicaoController {
     static async cadastrar(req, res) {
+        console.log('req.body:', req.body);  
+        console.log('req.session:', req.session);
         const { razao_social_instituicao, cnpj_instituicao } = req.body;
 
-        // Assumindo que o usuário esteja autenticado e que o ID do usuário esteja salvo na sessão
+        
         const usuarioId = req.session.userId;
 
         try {
             await InstituicaoModel.cadastrarInstituicao(razao_social_instituicao, cnpj_instituicao, usuarioId);
-            res.redirect('/alguma-rota-de-sucesso');
+            res.redirect('/publicacao');
         } catch (error) {
             console.error(error.message);
             res.status(500).send('Erro no servidor ao cadastrar a instituição.');
